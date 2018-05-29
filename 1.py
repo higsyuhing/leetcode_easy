3e0009f0cf6e857da869b99e4ca09c9700f917e0
Minjun Wu <higsyuhing@users.noreply.github.com>
2018-05-28 21:02:19 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,16 +7,20 @@ class Solution(object):
         """
         num_dic = {}
         for index in range(len(nums)): 
-            if nums[index] < target: 
-                if nums[index] in num_dic: 
-                    pass
-                else: 
-                    num_dic[nums[index]] = index
-                    pass
-                cor_val = target - nums[index]
-                if cor_val in num_dic: 
-                    return [num_dic[cor_val], index]
+            cor_val = target - nums[index]
+            if cor_val in num_dic: 
+                return [num_dic[cor_val], index]
+            if nums[index] in num_dic: 
+                pass
+            else: 
+                num_dic[nums[index]] = index
                 pass
             pass
         return []
+    
+    '''
+    this can be done by many other ways, like: 
+    - brute force: 2 nested loop
+    - sorted array with 2 pointers
+    '''
     
