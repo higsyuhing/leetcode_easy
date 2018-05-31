b71dc4082486fe1320d8266733ae412375f5260e
Minjun Wu <higsyuhing@users.noreply.github.com>
2018-05-31 16:17:28 -0500

    Update 101.py

--- 101.py
+++ 101.py
@@ -4,7 +4,47 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root == None: 
+            return True
+        stack1 = []
+        stack2 = []
+        temp1 = root.left
+        temp2 = root.right
+        while 1: 
+            while 1: 
+                if temp1 == None and temp2 == None: 
+                    break
+                    pass
+                elif temp1 == None and temp2 != None: 
+                    return False
+                elif temp1 != None and temp2 == None: 
+                    return False
+                elif temp1.val != temp2.val: 
+                    return False
+                else: 
+                    stack1.append(temp1)
+                    stack2.append(temp2)
+                    temp1 = temp1.left
+                    temp2 = temp2.right
+                    pass
+                pass
+            if len(stack1) > 0: 
+                temp1 = stack1.pop()
+                temp2 = stack2.pop()
+                temp1 = temp1.right
+                temp2 = temp2.left
+                pass
+            else: 
+                return True
 
+
+'''
 class Solution(object):
     def checkTree(self, leftnode, rightnode): 
         if leftnode == None and rightnode == None: 
@@ -31,4 +71,4 @@ class Solution(object):
         if root == None: 
             return True
         return self.checkTree(root.left, root.right)        
-            
+'''
