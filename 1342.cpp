73dc2aa912ddbf1d7caf2ab6bb189c743289eac5
Minjun Wu <higsyuhing@users.noreply.github.com>
2020-06-17 01:07:47 -0500

    Create 1342.cpp

---
+++ 1342.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfSteps (int num) {
+        if(num <= 0) return 0; 
+        int step = 0; 
+        while(num > 0){
+            if(num & 1) num--; 
+            else num = num >> 1; 
+            step++; 
+        }
+        return step; 
+    }
+};
